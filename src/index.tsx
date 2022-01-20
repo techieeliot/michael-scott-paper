7631e362ffe163d3b4d694cbdd77047d922c77a0
Eliot Sanford <eliot587@gmail.com>
2022-01-20 06:41:05 -0600

    feat: add store and provider
    - change title

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { createStoreHook, applyMiddleware, Store } from "react-redux";
+import { createStore, applyMiddleware, Store } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 import "./index.css";
 import App from "./App";
+import reducer from "./store/reducer";
 import reportWebVitals from "./reportWebVitals";
+
+const store: Store<WebsiteState, WebsiteAction> & {
+  dispatch: DispatchType;
+} = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
