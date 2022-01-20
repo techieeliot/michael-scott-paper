d8e1c54df7a46f22ebfc56d8d9a38104df4dee9e
Eliot Sanford <eliot587@gmail.com>
2022-01-20 07:13:01 -0600

    feat: wrap selector page in a form and add radio group for layout selection

--- src/components/Selector/index.tsx
+++ src/components/Selector/index.tsx
@@ -1,6 +1,16 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
-import { Layout, Row, Col, Button, Typography, Divider, Card } from "antd";
+import {
+  Layout,
+  Row,
+  Col,
+  Button,
+  Typography,
+  Divider,
+  Card,
+  Form,
+  Radio,
+} from "antd";
 
 import "./index.css";
 
@@ -8,124 +18,183 @@ const { Header, Content, Footer } = Layout;
 const { Title, Paragraph } = Typography;
 const { Meta } = Card;
 
-const Selector: FC = () => (
-  <Row style={{ textAlign: "left", height: "100vh", background: "#fff" }}>
-    <Col span={24} style={{ height: "100vh" }}>
-      <Header
-        style={{
-          background: "#fff",
-          margin: "5vh auto 0 auto",
-          height: "auto",
-          width: "100%",
-        }}
-      >
-        <div
-          style={{
-            padding: "24px 24px 0 24px",
-            margin: "0 auto",
-            width: "80%",
-          }}
-        >
-          <Title>Select a layout for your single page website</Title>
-        </div>
-      </Header>
-      <Content
-        className="site-layout"
-        style={{ padding: "24px 50px", background: "#fff" }}
-      >
-        <div
-          style={{
-            padding: "12px 24px 24px 24px",
-            margin: "0 auto",
-            minHeight: "95%",
-            width: "80%",
-          }}
-        >
-          <Row style={{ margin: "0 auto 24px auto" }}>
-            <Col>
-              <Paragraph>
-                When choosing your layout there are several things you can
-                customize
-              </Paragraph>
-              <ul>
-                <li>
-                  The background color, or you can use a large image as the
-                  background
-                </li>
-                <li>Each container block can contain images or custom text</li>
-                <li>Any text block can contain a background image or color</li>
-              </ul>
-            </Col>
-          </Row>
+const Selector: FC = () => {
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const onFinish = (values: unknown) => {
+    console.log("Success:", values);
+  };
 
-          <Row
-            gutter={[12, 48]}
-            justify="space-between"
-            style={{ margin: "5vh auto" }}
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const onFinishFailed = (errorInfo: unknown) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  // const onChange = (e: { target: { value: string } }) => {
+  //   console.log("radio checked", e.target.value);
+  // };
+  return (
+    <Row style={{ textAlign: "left", height: "100vh", background: "#fff" }}>
+      <Col span={24} style={{ height: "100vh" }}>
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+          style={{ width: "100%" }}
+        >
+          <Form.Item name="header">
+            <Header
+              style={{
+                background: "#fff",
+                margin: "5vh auto 0 auto",
+                height: "auto",
+                width: "100%",
+              }}
+            >
+              <div
+                style={{
+                  padding: "24px 24px 0 24px",
+                  margin: "0 auto",
+                  width: "80%",
+                }}
+              >
+                <Title>Select a layout for your single page website</Title>
+              </div>
+            </Header>
+          </Form.Item>
+          <Form.Item
+            name="columns"
+            rules={[
+              {
+                required: true,
+                message: "Please input your column selection!",
+              },
+            ]}
           >
-            <Col flex="1 1 25%">
-              <Card
-                style={{ width: 290 }}
-                cover={
-                  <img
-                    alt="example"
-                    // eslint-disable-next-line max-len
-                    src="https://user-images.githubusercontent.com/19453294/149674074-5679c4d1-5c86-4e0f-832b-e139ad25d6bd.png"
-                  />
-                }
+            <Content
+              className="site-layout"
+              style={{ padding: "24px 50px", background: "#fff" }}
+            >
+              <Radio.Group
+                style={{
+                  padding: "12px 24px 24px 24px",
+                  margin: "0 auto",
+                  minHeight: "95%",
+                  width: "80%",
+                }}
+                // onChange={onChange}
+                // value={value}
               >
-                <Meta description="Header - Two Columns" />
-              </Card>
-            </Col>
-            <Col flex="1 1 25%">
-              <Card
-                style={{ width: 290 }}
-                cover={
-                  <img
-                    alt="example"
-                    // eslint-disable-next-line max-len
-                    src="https://user-images.githubusercontent.com/19453294/149674073-f950522d-62ca-442d-9de9-e2731efff94b.png"
-                  />
-                }
-              >
-                <Meta description="Header - Three Columns" />
-              </Card>
-            </Col>
-            <Col flex="1 1 25%">
-              <Card
-                style={{ width: 290 }}
-                cover={
-                  <img
-                    alt="example"
-                    // eslint-disable-next-line max-len
-                    src="https://user-images.githubusercontent.com/19453294/149674075-d4493e60-d7a3-4286-9287-9819129c8c75.png"
-                  />
-                }
-              >
-                <Meta description="Header/Footer - Two Columns" />
-              </Card>
-            </Col>
-          </Row>
-        </div>
-      </Content>
-      <Footer style={{ textAlign: "right", padding: 24, background: "#fff" }}>
-        <div
-          style={{
-            padding: "24px",
-            margin: "0 auto",
-            minHeight: "95%",
-            width: "80%",
-          }}
-        >
-          <Divider />
+                <Row style={{ margin: "0 auto 24px auto" }}>
+                  <Col>
+                    <Paragraph>
+                      When choosing your layout there are several things you can
+                      customize
+                    </Paragraph>
+                    <ul>
+                      <li>
+                        The background color, or you can use a large image as
+                        the background
+                      </li>
+                      <li>
+                        Each container block can contain images or custom text
+                      </li>
+                      <li>
+                        Any text block can contain a background image or color
+                      </li>
+                    </ul>
+                  </Col>
+                </Row>
 
-          <Button type="primary" shape="round" size="large">
-            <Link to="/editor">Next</Link>
-          </Button>
-        </div>
-      </Footer>
-    </Col>
-  </Row>
-);
+                <Row
+                  gutter={[12, 48]}
+                  justify="space-between"
+                  style={{ margin: "5vh auto" }}
+                >
+                  <Col flex="1 1 25%">
+                    <Radio value="Header - Two Columns">
+                      <Card
+                        style={{ width: 290 }}
+                        cover={
+                          <img
+                            alt="example"
+                            // eslint-disable-next-line max-len
+                            src="https://user-images.githubusercontent.com/19453294/149674074-5679c4d1-5c86-4e0f-832b-e139ad25d6bd.png"
+                          />
+                        }
+                      >
+                        <Meta description="Header - Two Columns" />
+                      </Card>
+                    </Radio>
+                  </Col>
+                  <Col flex="1 1 25%">
+                    <Radio value="Header - Three Columns">
+                      <Card
+                        style={{ width: 290 }}
+                        cover={
+                          <img
+                            alt="example"
+                            // eslint-disable-next-line max-len
+                            src="https://user-images.githubusercontent.com/19453294/149674073-f950522d-62ca-442d-9de9-e2731efff94b.png"
+                          />
+                        }
+                      >
+                        <Meta description="Header - Three Columns" />
+                      </Card>
+                    </Radio>
+                  </Col>
+                  <Col flex="1 1 25%">
+                    <Radio value="Header/Footer - Two Columns">
+                      <Card
+                        style={{ width: 290 }}
+                        cover={
+                          <img
+                            alt="example"
+                            // eslint-disable-next-line max-len
+                            src="https://user-images.githubusercontent.com/19453294/149674075-d4493e60-d7a3-4286-9287-9819129c8c75.png"
+                          />
+                        }
+                      >
+                        <Meta description="Header/Footer - Two Columns" />
+                      </Card>
+                    </Radio>
+                  </Col>
+                </Row>
+              </Radio.Group>
+            </Content>
+          </Form.Item>
+          <Form.Item name="footer">
+            <Footer
+              style={{ textAlign: "right", padding: 24, background: "#fff" }}
+            >
+              <div
+                style={{
+                  padding: "24px",
+                  margin: "0 auto",
+                  minHeight: "95%",
+                  width: "80%",
+                }}
+              >
+                <Divider />
+
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  shape="round"
+                  size="large"
+                >
+                  <Link to="/editor">Next</Link>
+                </Button>
+              </div>
+            </Footer>
+          </Form.Item>
+        </Form>
+      </Col>
+    </Row>
+  );
+};
 
 export default Selector;
