870d3ea3e957ea1585707641e8f94b7cca7de935
Eliot Sanford <eliot587@gmail.com>
2022-01-22 01:51:50 -0600

    feature: add Upload component WIP
    - add id and classNames to a few components
    - attempt to create a handlechange
    - create an upload button sub-component
    - comment out the Empty and Button
    - allow upload to accept any image file
    - update the alt text on the image component

--- src/components/ContentBuilder/BackgroundMenu/index.tsx
+++ src/components/ContentBuilder/BackgroundMenu/index.tsx
@@ -1,9 +1,10 @@
 import { FC, useState, useEffect } from "react";
-import { Menu, Input, Empty, Button, Tooltip } from "antd";
+import { Menu, Input, Empty, Button, Tooltip, Upload, message } from "antd";
 import {
   LaptopOutlined,
   UserOutlined,
   PictureOutlined,
+  LoadingOutlined,
 } from "@ant-design/icons";
 
 import "./index.css";
@@ -13,6 +14,33 @@ const { SubMenu } = Menu;
 const BackgroundMenu: FC = () => {
   const initialHexValue = window.localStorage.getItem("hexValue") || "#000";
   const [hexValue, setHexValue] = useState<string>(initialHexValue);
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState("");
+
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const handleChange = (info: {
+    file: { status: string; originFileObj: Blob };
+  }) => {
+    if (info.file.status === "uploading") {
+      setLoading(true);
+      return;
+    }
+    if (info.file.status === "done") {
+      // Get this url from response in real world.
+      setLoading(false);
+    }
+  };
+
+  const uploadButton = (
+    <div id="upload-button-rectangle-component">
+      {loading ? (
+        <LoadingOutlined id="loading-outlined" style={{ fontSize: "60px" }} />
+      ) : (
+        <PictureOutlined id="picture-outlined" style={{ fontSize: "60px" }} />
+      )}
+      <div style={{ marginTop: 8 }}>select image</div>
+    </div>
+  );
 
   useEffect(() => {
     return window.localStorage.setItem("hexValue", hexValue);
@@ -63,13 +91,17 @@ const BackgroundMenu: FC = () => {
         </Menu.Item>
       </SubMenu>
       <SubMenu
-        className="image-submenu"
+        className="background-menu-image-submenu"
         key="sub4"
         icon={<LaptopOutlined />}
         title="Background Image"
       >
-        <Menu.Item key="2" style={{ height: "auto" }}>
-          <Button
+        <Menu.Item
+          key="2"
+          id="background-menu-menu-item-image-uploader"
+          style={{ height: "auto" }}
+        >
+          {/* <Button
             id="select-image-btn"
             type="link"
             style={{
@@ -86,7 +118,29 @@ const BackgroundMenu: FC = () => {
                 height: 60,
               }}
             />
-          </Button>
+          </Button> */}
+          <Upload
+            id="background-menu-upload-image"
+            name="background-image"
+            listType="picture-card"
+            className="background-uploader"
+            showUploadList={false}
+            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+            // beforeUpload={beforeUpload}
+            onChange={handleChange}
+            accept="image/*"
+          >
+            {imageUrl ? (
+              <img
+                id="background-menu-uploaded-image"
+                src={imageUrl}
+                alt="background to display in selected section"
+                style={{ width: "100%" }}
+              />
+            ) : (
+              uploadButton
+            )}
+          </Upload>
         </Menu.Item>
       </SubMenu>
     </Menu>
