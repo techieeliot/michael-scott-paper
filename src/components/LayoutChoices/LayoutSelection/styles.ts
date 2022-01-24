4f9f6ef10e853520bfce46db3b2893f626a9e124
Eliot Sanford <eliot587@gmail.com>
2022-01-24 03:26:54 -0600

    style: add radio group global style

--- src/components/LayoutChoices/LayoutSelection/styles.ts
+++ src/components/LayoutChoices/LayoutSelection/styles.ts
@@ -1,7 +1,27 @@
-import styled, { createGlobalStyled } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import { Radio } from "antd";
 
-// eslint-disable-next-line import/prefer-default-export
+export const RadioGlobalStyle = createGlobalStyle`
+/* HIDE RADIO */
+.select-column .ant-radio {
+  position: absolute;
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* IMAGE STYLES */
+.ant-radio-wrapper {
+  cursor: pointer;
+}
+
+/* CHECKED STYLES */
+.select-row .ant-radio-wrapper-checked .ant-card {
+  outline: 2px solid #1890ff;
+  margin: 0;
+}
+`;
+
 export const RadioGroup = styled(Radio.Group)`
   padding: 12px 24px 24px 24px;
   margin: 0 auto;
