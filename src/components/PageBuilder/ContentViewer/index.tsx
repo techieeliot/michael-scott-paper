42367700dd4166c006c500dfd9f139aa0f08554f
Eliot Sanford <eliot587@gmail.com>
2022-01-31 06:31:59 -0600

    feat: display layout matching the type

--- src/components/PageBuilder/ContentViewer/index.tsx
+++ src/components/PageBuilder/ContentViewer/index.tsx
@@ -1,13 +1,24 @@
 import { FC } from "react";
 import { Layout, Typography } from "antd";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import HeaderFooterTwoColumnsLayout from "./HeaderFooterTwoColumnsLayout";
-import HeaderThreeColumnsLayout from "./HeaderTwoColumnsLayout";
-import HeaderTwoColumnsLayout from "./HeaderThreeColumnsLayout";
+import HeaderThreeColumnsLayout from "./HeaderThreeColumnsLayout";
+import HeaderTwoColumnsLayout from "./HeaderTwoColumnsLayout";
+import { RootState } from "../../../store/configureStore";
+import { IWebsite } from "../../../store/interfaces/IWebsite";
 
 const { Content } = Layout;
 const { Paragraph } = Typography;
 
 const ContentLayout: FC = () => {
+  const params = useParams();
+  const websiteData = useSelector<RootState>((state) =>
+    state.rootReducer.websiteBuilder.websites.find(
+      (site) => site.id === params.id
+    )
+  ) as IWebsite;
+
   return (
     <Content
       id="content-layout"
@@ -26,9 +37,15 @@ const ContentLayout: FC = () => {
           background
         </Paragraph>
       )}
-      {false && <HeaderTwoColumnsLayout />}
-      {true && <HeaderThreeColumnsLayout />}
-      {false && <HeaderFooterTwoColumnsLayout />}
+      {websiteData.layout === "Header - Two Columns" && (
+        <HeaderTwoColumnsLayout />
+      )}
+      {websiteData.layout === "Header - Three Columns" && (
+        <HeaderThreeColumnsLayout />
+      )}
+      {websiteData.layout === "Header/Footer - Two Columns" && (
+        <HeaderFooterTwoColumnsLayout />
+      )}
     </Content>
   );
 };
